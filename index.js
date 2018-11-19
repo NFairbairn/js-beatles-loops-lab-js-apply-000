a1cd3e69f940981262717149480cd180958f1edf
Noah Fairbairn <noahfairbairn@gmail.com>
2018-11-19 16:17:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // add solution here
 
   function theBeatlesPlay(musicians, instruments) {
+//creates an empty array
   var empty = [];
-
+//adds counter, checks if counter is less than the quantity of musicians, incriments counter
   for(var i = 0; i < musicians.length; i++) {
-
+//pushes the concatenated string calling my arrays and counter to the empty array
   empty.push(`${musicians[i]} plays ${instruments[i]}`)
     }
   return empty
@@ -13,21 +14,29 @@
 
 
 function johnLennonFacts(facts) {
+//creates my counter
 var counter = 0;
+//checks if counter is less than how many facts are within the array
   while ( counter < facts.length) {
+//calls the next fact using the counter, and adds a '!!!' at the end of the string.
     facts[counter]+='!!!';
+//incriments the counter for the loop
     counter++;
 }
 return facts
 }
 
-
+//used the parameter 'number' because i need it to determine how long to run the loop
 function iLoveTheBeatles(number) {
   var emptyArray = [];
+
   do {
+//pushes the string to the empty array
      emptyArray.push('I love the Beatles!');
+//incriments the counter for the loop
      number++;
   }
+//sets parameter for the loop to keep going until its been executed 14 times
   while (number < 15);
 
   return emptyArray
